920a55c1f89d79831e2f7c891c7b9c6d5bc9c86a
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-15 15:38:56 +0100

    imported add friend component

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, NavLink, withRouter, Redirect } from 'react-router-dom';
 import Login from './components/Login';
 import Friends from './components/Friends';
+import AddFriend from './components/AddFriend';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ function App(props) {
         <span>
           <NavLink exact to='/api/login'>Login</NavLink>
           <NavLink to='/friends'>Friends</NavLink>
+          <NavLink to='/addFriend'>AddFriend</NavLink>
         </span>
 
         <button onClick={onLogout}>Logout</button>
@@ -29,7 +31,17 @@ function App(props) {
           exact
           path='/api/login'
           component={Login}
-        />
+        /> &nbsp;
+        <Route
+          exact
+          path='/addFriend'
+          render={props => {
+            if (localStorage.getItem('payload')) {
+              return <AddFriend {...props} />
+            }
+            return <Redirect to='/api/login' />
+          }}
+        /> &nbsp;
 
         {/* (OPTION B) Create a secure Route for Quotes.
         Alternatively, we could have the Quotes component
